c86a4405230d252abe9591af6c36f8a8d32ce316
dooking <123456ghghgh@naver.com>
2021-05-10 01:15:55 +0900

    feat: MainPage 및 Slider 완성

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { BsBucket, BsBarChart } from "react-icons/bs";
+import { Link } from 'react-router-dom';
+import * as S from './style'
 
 function Home() {
-	return <div>Home</div>;
+	return (
+		<S.HomeContainer>
+			<S.TitleDiv>
+				제목
+			</S.TitleDiv>
+			<S.MenuCard>
+				<Link to="/shop">
+					<S.MenuItem>
+						<S.MenuIcon>
+							<BsBucket />
+						</S.MenuIcon>
+						<S.MenuTitle>착한 업소 정보 얻기</S.MenuTitle>
+					</S.MenuItem>
+				</Link>
+				<Link to="/price">
+					<S.MenuItem>
+						<S.MenuIcon>
+							<BsBarChart />
+						</S.MenuIcon>
+						<S.MenuTitle>착한 물가 정보 얻기</S.MenuTitle>
+					</S.MenuItem>
+				</Link>
+			</S.MenuCard>
+		</S.HomeContainer>
+	);
 }
 
 export default Home;
--- client/src/components/navbar/style.js
+++ client/src/components/navbar/style.js
@@ -3,18 +3,17 @@ import styled from 'styled-components';
 export const NavBarWrapper = styled.div`
   display: flex;
   padding: 20px;
+  justify-content: space-between;
+  font-size: 2em;
+  font-weight: bold;
 `;
 
 export const Title = styled.div`
-  width: 20%;
+  text-decoration: none;
+  color: black;
 `;
 
 export const MenuWrapper = styled.div`
-  width: 80%;
-  display: flex;
-  justify-content: space-around;
+  cursor: pointer;
 `;
-export const MenuItem = styled.a`
-  border: 1px solid black;
-  padding: 10px;
-`;
+
