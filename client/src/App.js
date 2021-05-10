5a84860b075d32bb44b796c62fed882121466756
dooking <123456ghghgh@naver.com>
2021-05-10 20:22:39 +0900

    feat: web font 추가

--- client/src/App.js
+++ client/src/App.js
@@ -30,7 +30,8 @@ const GlobalStyle = createGlobalStyle`
   * {
     padding: 0px;
     margin: 0px;
-    font-family: 'Noto Sans KR', sans-serif;
+    /* font-family: 'Noto Sans KR', sans-serif; */
+    font-family: 'Hanna', fantasy;
     box-sizing: border-box;
   }
   a {
