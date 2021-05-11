6b75efabba7878ce3edbe6ed55da9ea1661307e0
longshiine <dkel03@korea.ac.kr>
2021-05-11 21:23:08 +0900

    feat: priceRouter데이터 형식 지정
    Signed-off-by: longshiine <dkel03@korea.ac.kr>

--- server/routes/priceRouter/controller.js
+++ server/routes/priceRouter/controller.js
@@ -1,23 +1,11 @@
 const axios = require('axios');
 const Configs = require('../../configs')
+const data = require('./data')
 
-exports.price = async (req, res, next) => {
+exports.getPriceAll = async (req, res, next) => {
   try {
-      const priceDataUrl = `http://apis.data.go.kr/B552895/LocalGovPriceInfoService/getItemPriceResearchSearch?serviceKey=${Configs.priceOptions.serviceKey}&pageNo=${Configs.priceOptions.pageNo}&numOfRows=${Configs.priceOptions.numOfRows}&examin_de=${Configs.priceOptions.examin_de}&prdlst_cd=${Configs.priceOptions.prdlst_cd}&prdlst_nm=${Configs.priceOptions.prdlst_nm}`;
-      axios.get(priceDataUrl)
-      .then((response) => {
-        const {
-          data: {
-            response: {
-              body: {
-                items: { item },
-              },
-            },
-          },
-        } = response;
-        res.send(item);
-      })
-      .catch((e) => console.log(e));
+      const items = data['DATA']
+      res.send(items)
   }
   catch(error){
       next(error);
---
+++ server/routes/priceRouter/data/index.js
@@ -0,0 +1,126 @@
+module.exports = {
+  "DESCRIPTION": {
+    m_seq: "판매처 코드",
+    gu_code: "구 코드",
+    market_name: "판매처 이름",
+    items: [
+      {
+        name: "품목 이름",
+        unit: "단위",
+        price: "가격",
+        etc: "원산지"
+      }
+    ]
+  },
+  "DATA": [
+    {
+      m_seq: 13,
+      gu_code: "성북구",
+      market_name: "돈암제일시장",
+      items: [
+        {
+          name: "사과(부사, 300g)",
+          unit: "1개",
+          price: 3000,
+          etc: "국내"
+        },
+        {
+          name:"배(신고, 600g)",	
+          unit: "1개",	
+          price: 5000,
+          etc: "국내"
+        },
+        {
+          name:"배추(2.5~3kg)",	
+          unit: "1포기",	
+          price: 3000,
+          etc: "국내"
+        },
+        {
+          name:"무",	
+          unit: "1개",	
+          price: 1500,
+          etc: "국내"
+        },
+        {
+          name:"양파(1.5kg망)",	
+          unit: "1망(5개)",	
+          price: 4000,
+          etc: "국내"
+        },
+        {
+          name:"상추",	
+          unit: "100g(300g 2,000원)",	
+          price: 666,          
+          etc: "국내"
+        },
+        {
+          name:"오이",	
+          unit: "1개 (1봉 5개 2,000원)",	
+          price: 400,          
+          etc: "국내"
+        },
+        {
+          name:"애호박",	
+          unit: "1개",	
+          price: 1000,
+          etc: "국내"
+        },
+        {
+          name:"쇠고기(한우,불고기)",	
+          unit: "600g",	
+          price: 28000,
+          etc: "국내"
+        },
+        {
+          name:"돼지고기(생삼겹살)",	
+          unit: "600g",	
+          price: 15000,
+          etc: "국내"
+        },
+        {
+          name:"닭고기(육계)",	
+          unit: "1kg",	
+          price: 5500,
+          etc: "국내"
+        },
+        {
+          name:"달걀(10개)",	
+          unit: "10개",	
+          price: 3600,
+          etc: "국내 천하명란"
+        },
+        {
+          name:"달걀(30개)",	
+          unit: "30개",	
+          price: 9000,
+          etc: "국내 특란"
+        },
+        {
+          name:"조기(생물,수입산)",	
+          unit: "1마리 (3마리 10000원)",	
+          price: 3333,
+          etc: "수입부세"
+        },
+        {
+          name:"명태(러시아,냉동)",	
+          unit: "1마리 (2마리 5,000원)",	
+          price: 2500,
+          etc: "러시아"
+        },
+        {
+          name:"오징어",	
+          unit: "1마리 (4마리 10000원)",	
+          price: 2500,
+          etc: "국내 총알"
+        },
+        {
+          name:"고등어",	
+          unit: "1마리 (2마리 5000원)",	
+          price: 2500,
+          etc: "국내 냉동"
+        },
+      ]
+    }
+  ]
+}
