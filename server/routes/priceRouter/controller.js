d384e6af34540f99b548799a985a9df305ff50ca
longshiine <dkel03@korea.ac.kr>
2021-05-12 23:52:51 +0900

    feat: api/price/guCode
    Signed-off-by: longshiine <dkel03@korea.ac.kr>

--- server/routes/priceRouter/controller.js
+++ server/routes/priceRouter/controller.js
@@ -1,11 +1,27 @@
-const axios = require('axios');
-const Configs = require('../../configs')
-const data = require('./data')
-
 const GU_DATA = require('./data/preprocess/json/GU_DATA.json')
 const MARKET_DATA = require('./data/preprocess/json/MARKET_DATA.json')
 const PRICE_DATA = require('./data/preprocess/json/PRICE_DATA.json')
 const AVERAGE_DATA = require('./data/preprocess/json/AVERAGE_DATA.json')
+
+exports.getGuCode = async (req, res, next) => {
+  try {
+      const items = GU_DATA['DATA']
+      res.send(items)
+  }
+  catch(error){
+      next(error);
+  }
+}
+
+exports.getMarket = async (req, res, next) => {
+  try {
+      const items = MARKET_DATA['DATA']
+      res.send(items)
+  }
+  catch(error){
+      next(error);
+  }
+}
 
 exports.getPriceAll = async (req, res, next) => {
   try {
@@ -26,3 +42,4 @@ exports.getPriceAverage = async (req, res, next) => {
       next(error);
   }
 }
+
--- server/routes/priceRouter/index.js
+++ server/routes/priceRouter/index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Controller = require("./controller");
 
-/* GET home page. */
+/* 구, 시장정보 관련 API. 
+ - /guCode = 서울시의 모든 '구'에 대한 코드를 리턴합니다.
+ - /market = 서울시의 모든 시장/마트에 대한 코드를 리턴합니다.
+*/
+
+router.get('/guCode', Controller.getGuCode);
+router.get('/market', Controller.getMarket);
+
+/* Price 관련 API. 
+ - /all = 모든 시장/마트의 가격 정보를 리턴합니다.
+ - /average = 평균(Median 중간값을 사용) 가격 정보를 리턴합니다.
+ - /:gu_code = 해당 '구'에 해당하는 시장/마트의 가격 정보를 리턴합니다.
+*/
+
 router.get('/all', Controller.getPriceAll);
 router.get('/average', Controller.getPriceAverage);
-
-router.get('/test', function(req, res, next) {
-  res.send("hello");
-});
+// router.get('/:gu_code', Controller.getPriceByGu)
 
 module.exports = router;
